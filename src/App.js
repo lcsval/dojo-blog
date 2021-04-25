4cfc7f3b79df68d28c50e8aa1cc5932d9220168f
Lucas Val <lucas_val@outlook.com>
2021-04-25 10:02:01 -0300

    Multiple components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // const person = { name: 'yoshi', age: 30 }
-  const link = "http://www.google.com";
+    return (
+        <div className="App">
+            <Navbar />
 
-  return (
-    <div className="App">
-     <div className="content">
-       <h1>{ title }</h1>
-
-       {/* <p>Liked { likes } times</p> */}
-
-       <p>{ 10 }</p>
-       <p>{ "Hello, ninjas" }</p>
-       <p>{ [1,2,3,4,5] }</p>
-       <p>{ Math.random() * 10}</p>
-
-       <a href={link}>Google Site</a>
-     </div>
-    </div>
-  );
+            <div className="content">
+                <Home />
+            </div>
+        </div>
+    );
 }
 
 export default App;
