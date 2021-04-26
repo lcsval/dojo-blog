0de688fc7e9e4f41c6bc3727e6c4f31e6503d8be
Lucas Val <lucas_val@outlook.com>
2021-04-26 17:08:54 -0300

    Submit Events

--- src/Create.js
+++ src/Create.js
@@ -5,11 +5,18 @@ const Create = () => {
     const [body, setbody] = useState('');
     const [author, setAuthor] = useState('mario');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        console.log(blog);
+    }
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input 
                     type="text"
