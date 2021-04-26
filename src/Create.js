817fe63f47c4d00d606184a974928b81c5966149
Lucas Val <lucas_val@outlook.com>
2021-04-26 17:18:49 -0300

    Programmatic redirects

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setbody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isLoading, setIsLoading] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -19,6 +21,8 @@ const Create = () => {
         }).then(() => {
             console.log("new blog added");
             setIsLoading(false);
+            //history.go(-1);
+            history.push('/');
         });
     }
 
